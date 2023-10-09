4c334762d7059a1fc65119a507372b6ddbe70a4f
Louis Pang <lpang@princeton.edu>
2023-10-08 22:51:14 -0400

    print statements

--- src/sim.cpp
+++ src/sim.cpp
@@ -232,7 +232,7 @@ int main(int argc, char** argv) {
                 regData.registers[rt] = regData.registers[rs] + signExtImm;
                 break;
             case OP_ADDIU: 
-                printf("ADDIU %d + %d\n", regData.registers[rs], signExtImm);
+                printf("ADDIU %d + %d to %d\n", regData.registers[rs], signExtImm, rt);
                 regData.registers[rt] = regData.registers[rs] + signExtImm;
                 break;
             case OP_ANDI: 
